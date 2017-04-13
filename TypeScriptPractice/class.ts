a4dcda7c93eb5e4aafcd500d8139a174a555a883
lindl <lin_donglian@163.com>
2017-04-13 18:03:58 +0800

    派生类

--- TypeScriptPractice/class.ts
+++ TypeScriptPractice/class.ts
@@ -24,3 +24,26 @@ console.log( 'Name of Shape: ' + square.name );
 console.log( 'Color of Shape: ' + square.color );
 console.log( 'Width of Shape: ' + square.width );
 console.log( 'Height of Shape: ' + square.height );
+
+
+class Shape3D extends Shape {
+
+    volume: number;
+
+    constructor ( public name: string, width: number, height: number, length: number ) {
+        super( name, width, height );
+        this.volume = length * this.area;
+    };
+
+    shoutout() {
+        return "I'm " + this.name +  " with a volume of " + this.volume + " cm cube.";
+    }
+
+    superShout() {
+        return super.shoutout();
+    }
+}
+
+var cube = new Shape3D("cube", 30, 30, 30);
+console.log( cube.shoutout() );
+console.log( cube.superShout() );
