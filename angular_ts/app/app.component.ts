487384dde477b8754cb1a6af5b74f623d8d79bb8
lindl <lin_donglian@163.com>
2017-04-15 18:04:01 +0800

    基于TypeScript的Angular2 Hello World

---
+++ angular_ts/app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by lin on 2017/4/15.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>我的第一个 Angular 应用</h1>'
+})
+export class AppComponent { }
---
+++ angular_ts/app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by lin on 2017/4/15.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
