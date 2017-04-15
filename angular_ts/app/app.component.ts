24c5872ff9ef6bf49b66dab3947fb72fabc4ab90
lindl <lin_donglian@163.com>
2017-04-15 19:07:14 +0800

    使用 ngFor 显示数组属性

--- angular_ts/app/app.component.ts
+++ angular_ts/app/app.component.ts
@@ -4,10 +4,19 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template:
-        '<h1>{{title}}</h1> <h2>我喜欢的网站: {{mySite}}</h2>'
+    template:`
+        <h1>{{title}}</h1> 
+        <h2>我喜欢的网站: {{mySite}}</h2>
+        <p>网站列表:</p>
+        <ul>
+            <li *ngFor="let site of sites">
+                {{ site }}
+            </li>
+        </ul>
+        `
 })
 export class AppComponent {
     title='Angular插值';
     mySite='LinDL站点';
+    sites=['CSDN','掘金','简书'];
 }
